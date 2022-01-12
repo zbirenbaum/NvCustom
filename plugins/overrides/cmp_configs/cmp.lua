379b510243f2dc3de28df4288485e59a228dabf8
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-01-12 17:40:39 -0500

    fix for cmp conf

--- plugins/overrides/cmp_configs/cmp.lua
+++ plugins/overrides/cmp_configs/cmp.lua
@@ -24,7 +24,6 @@ vim.opt.completeopt = "menuone,noselect"
  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = {
        prefix = "",
-       spacing = 8,
     },
     signs = true,
     underline = true,
