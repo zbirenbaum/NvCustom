bde395b58e1ae1b15561d1d7c60ba829b4104c14
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-01-14 16:33:00 -0500

    updated for lightspeed deprecations

--- plugins/custom_plugin_configs/lightspeed.lua
+++ plugins/custom_plugin_configs/lightspeed.lua
@@ -1,18 +1,18 @@
 require("lightspeed").setup {
-   --jump_to_first_match = true,
-   jump_on_partial_input_safety_timeout = 400,
-   -- This can get _really_ slow if the window has a lot of content,
-   -- turn it on only if your machine can always cope with it.
-   highlight_unique_chars = false,
-   grey_out_search_area = true,
-   match_only_the_start_of_same_char_seqs = true,
-   limit_ft_matches = 50,
---   full_inclusive_prefix_key = "<C-SPACE>",
---   Lightspeed_x = "<C-f>",
---   Lightspeed_X = "<C-f>",
-   -- By default, the values of these will be decided at runtime,
-   -- based on `jump_to_first_match`.
-   labels = nil,
-   cycle_group_fwd_key = nil,
-   cycle_group_bwd_key = nil,
+	--jump_to_first_match = true,
+	jump_to_unique_chars = { safety_timeout = 400 },
+	-- This can get _really_ slow if the window has a lot of content,
+	-- turn it on only if your machine can always cope with it.
+	--   highlight_unique_chars = false,
+	--   grey_out_search_area = true,
+	match_only_the_start_of_same_char_seqs = true,
+	limit_ft_matches = 50,
+	--   full_inclusive_prefix_key = "<C-SPACE>",
+	--   Lightspeed_x = "<C-f>",
+	--   Lightspeed_X = "<C-f>",
+	-- By default, the values of these will be decided at runtime,
+	-- based on `jump_to_first_match`.
+	labels = nil,
+	cycle_group_fwd_key =nil,
+	cycle_group_bwd_key = nil,
 }
