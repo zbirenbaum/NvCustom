614dc3d7f0856f0586e299379794c1ce6e23a0a0
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-02-10 03:53:47 -0500

    cmp fix for neovim development

--- plugins/completion_plugins/cmp_configs/cmp.lua
+++ plugins/completion_plugins/cmp_configs/cmp.lua
@@ -71,11 +71,11 @@ cmp.setup {
       ghost_text = true,
    },
    sources = {
-      { name = "nvim_lsp", group_index = 1},
-      { name = "path", group_index = 1},
+      { name = "nvim_lsp", group_index = 2},
+      { name = "path", group_index = 2},
 --      { name = "luasnip", group_index = 2 },
 --      { name = "buffer", group_index = 3 },
-      { name = "nvim_lua", group_index = 2 },
+      { name = "nvim_lua", group_index = 1 },
    },
    sorting = {
       comparators = {
