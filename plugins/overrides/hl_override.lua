3a9e6e84216bbaaeb80a46fbb4686eaee12d03f9
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-02-19 23:26:36 -0500

    move Pmenu hl to hl-override for consistency

--- plugins/overrides/hl_override.lua
+++ plugins/overrides/hl_override.lua
@@ -16,3 +16,6 @@ end
 
 bg("TabLineFill", "NONE")
 bg("TabLine", "#000000")
+
+vim.cmd [[highlight! Pmenu guibg=#10171f]]
+vim.cmd [[highlight! PmenuSel guibg=NONE guifg=NONE gui=underline guisp=#569CD6]]
