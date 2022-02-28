b6900ca1fcb13470bb7ec8f0464067da7189161a
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-02-28 04:33:03 -0500

    removed debug

--- test_hl.lua
+++
@@ -1,7 +0,0 @@
-local fn = vim.fn
-
-local function get_color(group, attr)
-    return fn.synIDattr(fn.synIDtrans(fn.hlID(group)), attr)
-end
-
-print(fn.synIDattr(fn.synIDtrans(fn.hlID("Normal")), "fg"))
