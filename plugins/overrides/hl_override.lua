8a2bf91de71ffda80f9b734490decf065178121f
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-03-03 15:00:17 -0500

    autopush

--- plugins/overrides/hl_override.lua
+++ plugins/overrides/hl_override.lua
@@ -3,14 +3,27 @@ local colors = require("custom.colors").get()
 
 
 local violet = "#a038ba"
-local teal = "#54d7cb"
-local darkorange = "#c16c17"
-teal = "#16b0b6"
-vim.api.nvim_set_hl(0, "TSVariable", {fg=teal})
-vim.api.nvim_set_hl(0, "TSField", {fg="#d29355"})
-vim.api.nvim_set_hl(0, "TSKeyWord", {fg="#fba8c9"})
-vim.api.nvim_set_hl(0, "TSRepeat", {fg="#fba8c9"})
+local magenta = "#E753B3"
+local paleteal = "#D1BCEE"
+local dark_magenta = "#F490AF"
+local forest_green = "#099E31"
+local rust = "#D47D19"
+-- local teal = "#54d7cb"
+-- local darkorange = "#c16c17"
+-- teal = "#16b0b6"
+-- vim.api.nvim_set_hl(0, "TSVariable", {fg=teal})
 
+-- local violet = "#a038ba"
+-- local teal = "#54d7cb"
+vim.api.nvim_set_hl(0, "TSVariable", {fg=paleteal})
+-- vim.api.nvim_set_hl(0, "TSField", {fg=orange})
+-- vim.api.nvim_set_hl(0, "TSKeyWord", {fg=violet})
+vim.api.nvim_set_hl(0, "TSKeyWordFunction", {fg=dark_magenta})
+--vim.api.nvim_set_hl(0, "TSField", {fg=magenta})
+vim.api.nvim_set_hl(0, "TSField", {fg=rust})
+vim.api.nvim_set_hl(0, "TSParameter", {fg=dark_magenta})
+-- vim.api.nvim_set_hl(0, "TSKeyWord", {fg="#fba8c9"})
+-- vim.api.nvim_set_hl(0, "TSRepeat", {fg="#fba8c9"})
 
 local function bg(group, color)
    cmd("hi " .. group .. " guibg=" .. color)
