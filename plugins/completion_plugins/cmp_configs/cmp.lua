0974d76bc195d6cf672c4a3e49107764f23dfcef
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-04-03 17:05:34 -0400

    fix

--- plugins/completion_plugins/cmp_configs/cmp.lua
+++ plugins/completion_plugins/cmp_configs/cmp.lua
@@ -74,7 +74,7 @@ cmp.setup {
    },
    sources = {
       { name = "copilot", group_index = 1},
-      { name = "nvim_lsp", group_index = 3},
+      { name = "nvim_lsp", group_index = 1},
       { name = "path", group_index = 2},
       -- { name = "luasnip", group_index = 2 },
       -- { name = "buffer", group_index = 5 },
